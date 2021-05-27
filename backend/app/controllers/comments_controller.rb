3d48e0c0e3b4ed3f231a4d1d0f7959380ae80f99
krae <kraewind@gmail.com>
2021-05-27 14:21:11 -0400

    Added to comments controller

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,5 +1,42 @@
 class CommentsController < ApplicationController
 
+      def index
+        comments = Comment.all
+        render json: comments
+      end
     
+      def show
+        render json: comment
+      end
+    
+      def create
+        comment = Comment.new(comment_params)
+        if comment.save
+          render json: comment
+        else
+          #errors
+        end
+      end
+    
+      def update
+        if comment.update(comment_params)
+          render json: comment
+        else
+          #errors
+        end
+      end
+    
+      def destroy
+        if comment.destroy
+          render json: {message: "Comment destroyed"}
+        else
+          render json: {message: "Errors: #{comment.errors.full_messages}"}
+        end
+      end
+    
+      private
+        def comment_params
+          params.require(:comment).permit(:content)
+        end
 
 end
