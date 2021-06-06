ec354a8f2f11b73d4ad3d649dd2d80d26886f23f
krae <kraewind@gmail.com>
2021-06-06 15:36:25 -0400

    Stubbed out comments.js

---
+++ frontend/javascripts/models/comment.js
@@ -0,0 +1,23 @@
+class Comment {
+    static all = []
+
+    constructor({author, id, med_id, content}) {
+        this.author = author
+        this.id = id
+        this.med_id = med_id
+        this.content = content
+        Comment.all.push(this)
+    }
+ 
+     static findById(id) {
+         return this.all.find(comment => comment.id === id)
+     }
+ 
+     static findOrCreateBy(commentObj) {
+         return this.findByName(commentObj.name) || new Comment(commentObj)
+     }
+
+    render() {
+            
+    }
+}
